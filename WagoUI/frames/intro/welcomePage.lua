18d64586f3e677c5d274482c25b32e7134d9ae1e
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-06-18 02:20:10 +0200

    dropdown or label for uiPack selection

--- WagoUI/frames/intro/welcomePage.lua
+++ WagoUI/frames/intro/welcomePage.lua
@@ -9,21 +9,32 @@ local function createPage()
 
   local header = DF:CreateLabel(page, string.format(L["Welcome to |c%sWago|rUI:"], addon.color), 38,
     "white");
-  header:SetWidth(page:GetWidth() - 10)
   header:SetJustifyH("CENTER")
-  header:SetPoint("TOP", page, "TOP", 0, -100);
+  header:SetPoint("TOP", page, "TOP", 0, -75);
+
+  -- label or dropdown, depending on if single or multiple ui packs available
+  local dropdownData = addon:GetWagoDataForDropdown()
+  if #dropdownData > 1 then
+    local uiPackDropdown = addon.DF:CreateDropdown(page, 250, 40, 20,
+      function() return addon:GetWagoDataForDropdown() end)
+    uiPackDropdown:SetPoint("TOP", header, "BOTTOM", 0, -10)
+  else
+    local uiPackLabel = DF:CreateLabel(page, dropdownData[1].label, 38, "white");
+    uiPackLabel:SetJustifyH("CENTER")
+    uiPackLabel:SetPoint("TOP", header, "BOTTOM", 0, -10)
+  end
 
   local logo = DF:CreateImage(page, [[Interface\AddOns\]]..addonName..[[\media\wagoLogo512]], 256, 256)
-  logo:SetPoint("TOP", header, "BOTTOM", 0, 50)
+  logo:SetPoint("TOP", header, "BOTTOM", 0, -20)
 
   local startButton = addon.DF:CreateButton(page, 230, 50, "Full Installation", 22)
-  startButton:SetPoint("BOTTOMLEFT", page, "BOTTOMLEFT", 140, 100);
+  startButton:SetPoint("BOTTOMLEFT", page, "BOTTOMLEFT", 140, 80);
   startButton:SetClickFunction(function()
     addon:NextPage()
   end);
 
   local expertButton = addon.DF:CreateButton(page, 230, 50, "Expert Mode", 22)
-  expertButton:SetPoint("BOTTOMRIGHT", page, "BOTTOMRIGHT", -140, 100);
+  expertButton:SetPoint("BOTTOMRIGHT", page, "BOTTOMRIGHT", -140, 80);
   expertButton:SetClickFunction(function()
     addon.frames.introFrame:Hide()
     addon.frames.profileFrame:Show()
