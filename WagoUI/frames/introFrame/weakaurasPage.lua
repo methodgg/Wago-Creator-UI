93283524f3b958234489984807719c188b7ba8c1
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-06-19 01:41:59 +0200

    weakaura list

--- WagoUI/frames/introFrame/weakaurasPage.lua
+++ WagoUI/frames/introFrame/weakaurasPage.lua
@@ -7,13 +7,31 @@ local pageName = "WeakAurasPage"
 local function createPage()
   local page = addon:CreatePageProtoType(pageName)
 
-  local header = DF:CreateLabel(page, "WeakAuras", 38, "white");
+  local text = "Install the WeakAuras you would like to use.\nYou can pick and choose from the list below!"
+  local header = DF:CreateLabel(page, text, 22, "white");
   header:SetWidth(page:GetWidth() - 10)
-  header:SetJustifyH("CENTER")
-  header:SetPoint("TOP", page, "TOP", 0, -100);
+  header:SetPoint("TOPLEFT", page, "TOPLEFT", 0, -15);
 
-  local profileList = addon.DF:CreateProfileList(page, page:GetWidth(), page:GetHeight())
-  -- profileList.UpdateData({})
+  local profileList = addon.DF:CreateProfileList(page, page:GetWidth(), page:GetHeight() - 105)
+  local updateData = function(data)
+    local filtered = {}
+    if data then
+      for _, entry in ipairs(data) do
+        if entry.moduleName == "WeakAuras" or entry.moduleName == "Echo Raid Tools" then
+          tinsert(filtered, entry)
+        end
+      end
+      --sort weakauras on top
+      table.sort(filtered, function(a, b)
+        local orderA = a.moduleName == "WeakAuras" and 1 or 0
+        local orderB = b.moduleName == "WeakAuras" and 1 or 0
+        return orderA > orderB
+      end)
+    end
+    profileList.updateData(filtered)
+  end
+  addon:RegisterDataConsumer(updateData)
+  addon:UpdateRegisteredDataConsumers()
   profileList.header:SetPoint("TOPLEFT", page, "TOPLEFT", 0, -80)
 
   return page
