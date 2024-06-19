ba38e589440cf6ff1e0e5a8024a66879301e8d38
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-06-19 22:06:27 +0200

    typo

--- WagoUI/frames/introFrame/weakaurasPage.lua
+++ WagoUI/frames/introFrame/weakaurasPage.lua
@@ -13,7 +13,7 @@ end
 local function createPage()
   local page = addon:CreatePageProtoType(pageName)
 
-  local text = L["Install the WeakAuras you would like to use.\nYou can may and choose from the list below!"]
+  local text = L["Install the WeakAuras you would like to use.\nYou may and choose from the list below!"]
   local header = DF:CreateLabel(page, text, 22, "white");
   header:SetWidth(page:GetWidth() - 10)
   header:SetPoint("TOPLEFT", page, "TOPLEFT", 0, -15);
