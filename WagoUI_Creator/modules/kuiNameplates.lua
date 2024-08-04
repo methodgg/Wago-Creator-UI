171e45828d7aa7ea78f5cbe55865a6f8dc965d1d
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-08-04 22:20:39 +0200

    Kui Nameplates module

---
+++ WagoUI_Creator/modules/kuiNameplates.lua
@@ -0,0 +1,39 @@
+local _, addon = ...
+local moduleName = "Kui Nameplates"
+local LAP = LibStub:GetLibrary("LibAddonProfiles")
+local lapModule = LAP:GetModule(moduleName)
+
+local function dropdownOptions(index)
+  local res = {}
+  if not lapModule.isLoaded() then return res end
+  local profileKeys = lapModule.getProfileKeys()
+  local currentProfileKey = lapModule.getCurrentProfileKey()
+  return addon.ModuleFunctions:CreateDropdownOptions(moduleName, index, res, profileKeys, currentProfileKey)
+end
+
+local function hookRefresh()
+  if not lapModule.isLoaded() then return end
+  --setting, deleting, and changing profiles
+  hooksecurefunc(KuiNameplatesCore, "ConfigChanged", function(_, k, v)
+    if not v then
+      addon:RefreshAllProfileDropdowns()
+    end
+  end)
+  --importing profiles
+  hooksecurefunc(KuiNameplatesCore.config, "PostProfile", function()
+    addon:RefreshAllProfileDropdowns()
+  end)
+end
+
+---@type ModuleConfig
+local moduleConfig = {
+  moduleName = moduleName,
+  lapModule = lapModule,
+  dropdownOptions = dropdownOptions,
+  copyFunc = nil,
+  hookRefresh = hookRefresh,
+  copyButtonTooltipText = nil,
+  sortIndex = 22,
+}
+
+addon.ModuleFunctions:InsertModuleConfig(moduleConfig)
