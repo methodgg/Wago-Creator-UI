c99569ecdb3d6a94d789152828481aa22d7f5ab9
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-08-08 23:40:47 +0200

    clarify getProfileAssignments documentation

--- WagoUI_Libraries/LibAddonProfiles/modules/ExampleModule.lua
+++ WagoUI_Libraries/LibAddonProfiles/modules/ExampleModule.lua
@@ -22,7 +22,7 @@ if (not private) then return; end
 ---@field closeConfig fun(self: LibAddonProfilesModule) : nil
 ---@field getProfileKeys? fun(self: LibAddonProfilesModule,) : table<string, any>
 ---@field getCurrentProfileKey? fun(self: LibAddonProfilesModule,) : string
----@field getProfileAssignments? fun(self: LibAddonProfilesModule) : table<string, string> | nil The key should be in format "Playername - RealmName". The value should be the profile key.
+---@field getProfileAssignments? fun(self: LibAddonProfilesModule) : table<string, string> | nil The key should be in format "Playername - RealmName". The value should be the profile key. A nil return value means that the profile not character specific and can not be directly retrieved. Use WagoUI db to get the profile key. If the function is not defined the module has global profiles.
 ---@field isDuplicate fun(self: LibAddonProfilesModule, profileKey: string) : boolean
 ---@field setProfile? fun(self: LibAddonProfilesModule, profileKey: string)
 ---@field testImport fun(self: LibAddonProfilesModule, profileString: string, profileKey: string | nil, profileData: table| nil, rawData: table | nil, moduleName: string | nil) : string | table | nil Test the profile string to see if it can be imported. Return the profile key if it can, nil otherwise. Tests profileData, rawData, and moduleName if they are provided, otherwise decodes the profileString and tests that.
