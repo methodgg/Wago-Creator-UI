62d0033437780464af0a2cdb732d7302248c7eb0
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-08-08 21:14:20 +0200

    Alt Frame UX

--- WagoUI/frames/altFrame/altFrame.lua
+++ WagoUI/frames/altFrame/altFrame.lua
@@ -3,28 +3,47 @@ local addonName = ...
 ---@class WagoUI
 local addon = select(2, ...)
 local DF = _G["DetailsFramework"];
+local LWF = LibStub("LibWagoFramework")
 local L = addon.L
 local LAP = LibStub:GetLibrary("LibAddonProfiles")
 
+local currentSelectedCharacter = nil
+
 local function setAllProfilesAsync()
-  local target = addon:GetImportedProfilesTarget()
-  for moduleName, data in pairs(target) do
-    ---@type LibAddonProfilesModule
-    local lap = LAP:GetModule(moduleName)
-    if data.profileKey and lap:isLoaded() then
-      if lap:needsInitialization() then
-        lap:openConfig()
-        C_Timer.After(0, function()
-          lap:closeConfig()
-        end)
+  print(currentSelectedCharacter)
+  -- local target = addon:GetImportedProfilesTarget()
+  -- for moduleName, data in pairs(target) do
+  --   ---@type LibAddonProfilesModule
+  --   local lap = LAP:GetModule(moduleName)
+  --   if data.profileKey and lap:isLoaded() then
+  --     if lap:needsInitialization() then
+  --       lap:openConfig()
+  --       C_Timer.After(0, function()
+  --         lap:closeConfig()
+  --       end)
+  --     end
+  --     local profileKeys = lap:getProfileKeys()
+  --     if profileKeys[data.profileKey] then
+  --       lap:setProfile(data.profileKey)
+  --     end
+  --   end
+  --   coroutine.yield()
+  -- end
+end
+
+local function getImportedProfilesDataForDropdown()
+  local res = {}
+  for key, data in pairs(addon.db.importedProfiles) do
+    local entry = {
+      value = key,
+      label = key,
+      onclick = function()
+        currentSelectedCharacter = key
       end
-      local profileKeys = lap:getProfileKeys()
-      if profileKeys[data.profileKey] then
-        lap:setProfile(data.profileKey)
-      end
-    end
-    coroutine.yield()
+    }
+    tinsert(res, entry)
   end
+  return res
 end
 
 function addon:CreateAltFrame(f)
@@ -34,30 +53,51 @@ function addon:CreateAltFrame(f)
   addon.frames.altFrame = altFrame
 
   local logo = DF:CreateImage(altFrame, [[Interface\AddOns\]]..addonName..[[\media\wagoLogo512]], 256, 256)
-  logo:SetPoint("TOP", altFrame, "TOP", 0, -60)
+  logo:SetPoint("TOP", altFrame, "TOP", 0, -15)
 
   local text = L["altFrameHeader"]
   local header = DF:CreateLabel(altFrame, text, 22, "white");
-  header:SetWidth(altFrame:GetWidth() - 10)
+  header:SetWidth(altFrame:GetWidth() - 100)
   header:SetJustifyH("CENTER")
-  header:SetPoint("LEFT", altFrame, "LEFT", 0, -80);
+  header:SetPoint("TOP", logo, "BOTTOM", 0, 5);
   addon.frames.altFrame.header = header
 
-  local cancelButton = addon.DF:CreateButton(altFrame, 220, 50, L["Cancel"], 22)
-  cancelButton:SetPoint("CENTER", altFrame, "CENTER", 160, -180)
+  -- label or dropdown, depending on if single or multiple characters exist that we imported profiles to
+  local dropdownData = getImportedProfilesDataForDropdown()
+  local dropdownFunc = function() return getImportedProfilesDataForDropdown() end
+  local uiPackDropdown = LWF:CreateDropdown(altFrame, 250, 50, 16, 1.5, dropdownFunc)
+  local key = dropdownData[1] and dropdownData[1].key or nil
+  uiPackDropdown:Select(key)
+  currentSelectedCharacter = key
+  uiPackDropdown:SetPoint("RIGHT", altFrame, "CENTER", -10, -100)
+  local sourceLabel = DF:CreateLabel(altFrame, L["Source:"], 14)
+  sourceLabel:SetPoint("BOTTOMLEFT", uiPackDropdown, "TOPLEFT", 0, 2)
+
+  local cancelButton = LWF:CreateButton(altFrame, 200, 40, L["Cancel"], 18)
+  cancelButton:SetPoint("BOTTOM", altFrame, "BOTTOM", 0, 40)
   cancelButton:SetClickFunction(function()
     addon.frames.altFrame:Hide()
     addon.frames.expertFrame:Show()
   end);
 
-  local setProfilesButton = addon.DF:CreateButton(altFrame, 220, 50, L["Set all Profiles"], 22)
-  setProfilesButton:SetPoint("CENTER", altFrame, "CENTER", -160, -180)
+  local setProfilesButton = LWF:CreateButton(altFrame, 220, 50, L["Load Profiles"], 22)
+  setProfilesButton:SetPoint("LEFT", uiPackDropdown, "RIGHT", 40, 0)
   setProfilesButton:SetClickFunction(function()
     addon:Async(function()
       setAllProfilesAsync()
       header:SetText(L["altFrameHeader2"])
+      if uiPackDropdown then uiPackDropdown:Hide() end
+      sourceLabel:Hide()
+      cancelButton:Hide()
+      setProfilesButton:ClearAllPoints()
+      setProfilesButton:SetPoint("CENTER", altFrame, "CENTER", 0, -100)
+      setProfilesButton:SetScale(1.2)
       setProfilesButton:SetText(L["Reload UI"])
       setProfilesButton:SetClickFunction(function()
+        --TODO TEMPORARY REMOVE THIS ONLY FOR TEST PURPOSES
+        --TODO TEMPORARY REMOVE THIS ONLY FOR TEST PURPOSES
+        --TODO TEMPORARY REMOVE THIS ONLY FOR TEST PURPOSES
+        WagoUI.dbC.hasLoggedIn = false
         ReloadUI()
       end);
     end);
