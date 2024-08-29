5b1920e8893adb0ed81507958ae5f3447e455035
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-08-29 20:26:02 +0200

    Formatting

--- definitions.lua
+++ definitions.lua
@@ -8,6 +8,7 @@
 ---@class RefreshHook : table
 ---@field tableFunc fun() : table The target table that we want to hook functions on
 ---@field functionNames table<number, string> The names of the functions that we want to hook
+
 ---@class LibAddonProfilesModule : table
 ---@field moduleName string The name of the module. This should be the same as the AddOn name but can be different if the AddOn has multiple modules.
 ---@field wagoId string | nil X-Wago-ID for the AddOn. If the addon is not on Wago, this should be nil.
@@ -40,6 +41,7 @@
 ---@field exportGroup? fun(self: LibAddonProfilesModule, profileKey: string) : string | nil For modules that have a group of export strings, export a single export string of that group. (e.g. WeakAuras)
 ---@field areProfileStringsEqual fun(self: LibAddonProfilesModule, profileStringA: string | nil, profileStringB: string | nil , tableA : table | nil, tableB : table | nil) : areEqual: boolean, changedEntries: table | nil, removedEntries: table | nil Deep compare two profile strings or two tables.
 ---@field refreshHookList table<number, RefreshHook> | nil Defines what functions should be hooked when wanting to monitor additions / deletions of profiles and changes to the currently active profile key.
+
 ---@class AddonResolutionEntry
 ---@field value string
 ---@field displayNameLong string
@@ -47,9 +49,11 @@
 ---@field width number | nil
 ---@field height number | nil
 ---@field defaultEnabled boolean
+
 ---@class AddonResolutions
 ---@field entries table<number, AddonResolutionEntry>
 ---@field defaultValue string
+
 ---@class LibDeflateAsync
 ---@field CompressDeflate fun(self: LibDeflateAsync, input: string, options: table): string
 ---@field EncodeForPrint fun(self: LibDeflateAsync, input: string): string)
@@ -57,30 +61,37 @@
 ---@field DecodeForPrint fun(self: LibDeflateAsync, input: string): string
 ---@field DecodeForWoWAddonChannel fun(self: LibDeflateAsync, input: string): string
 ---@field DecompressDeflate fun(self: LibDeflateAsync, input: string): string
+
 ---@class LibCompress
 ---@field Decompress fun(self: LibCompress, input: string): string
 ---@field GetAddonEncodeTable fun(self: LibCompress): table
 ---@field CompressHuffman fun(self: LibCompress, input: string): string
 ---@field DecompressHuffman fun(self: LibCompress, input: string): string
+
 ---@class AceSerializer-3.0Async
 ---@field Serialize fun(self: AceSerializer-3.0Async, input: any): string
 ---@field Deserialize fun(self: AceSerializer-3.0Async, input: string): boolean, table | nil
+
 ---@class LibSerialize
 ---@field SerializeAsyncEx fun(self:LibSerialize, configForLS: table | nil, inTable: table) : function
 ---@field DeserializeAsync fun(self:LibSerialize, serialized: string) : function
+
 ---@class LibAsync : table
 ---@field GetHandler fun(self, config: LibAsyncConfig | nil) : LibAsyncHandler
+
 ---@class LibAsyncConfig
 ---@field type "everyFrame" The type of handler to create.
 ---@field maxTime number The maximum time in milliseconds to spend on a single update.
 ---@field maxTimeCombat number The maximum time in milliseconds to spend on a single update while in dungeon combat.
 ---@field errorHandler fun(msg: string, stacktrace?: string, name?: string) The error handler to use when a coroutine errors.
+
 ---@class LibAsyncHandler
 ---@field size number
 ---@field frame table
 ---@field update table
 ---@field CancelAsync fun(self, name: string)
 ---@field Async fun(self, func: function, name: string, singleton: boolean)
+
 ---@class IntroImportState
 ---@field checked boolean
 ---@field profileMetadata table
