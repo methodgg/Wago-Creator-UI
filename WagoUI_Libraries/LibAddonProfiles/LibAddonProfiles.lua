e5526b5045aaf0546f0faf267a11b1f39214c395
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2024-12-03 02:35:08 +0100

    bump minor

--- WagoUI_Libraries/LibAddonProfiles/LibAddonProfiles.lua
+++ WagoUI_Libraries/LibAddonProfiles/LibAddonProfiles.lua
@@ -1,7 +1,7 @@
 ---@class LAPLoadingNamespace
 local loadingAddonNamespace = select(2, ...)
 local MAJOR = "LibAddonProfiles"
-local MINOR = 9
+local MINOR = 10
 
 ---@class LibAddonProfiles
 local LibAddonProfiles = LibStub:NewLibrary(MAJOR, MINOR)
