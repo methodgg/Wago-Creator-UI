3fd9d65b7204babb8c017cfba4b82091de4ae16d
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-01-18 23:25:39 +0100

    MPlusTimer

---
+++ WagoUI_Libraries/LibAddonProfiles/modules/MPlusTimer.lua
@@ -0,0 +1,88 @@
+local _, loadingAddonNamespace = ...
+---@type LibAddonProfilesPrivate
+local private = loadingAddonNamespace.GetLibAddonProfilesInternal and loadingAddonNamespace:GetLibAddonProfilesInternal()
+if (not private) then return end
+
+---@type LibAddonProfilesModule
+local m = {
+  moduleName = "MPlusTimer",
+  wagoId = "BKpqWZGE",
+  oldestSupported = "1.0.13",
+  addonNames = { "MPlusTimer" },
+  conflictingAddons = {},
+  icon = C_AddOns.GetAddOnMetadata("MPlusTimer", "IconTexture"),
+  slash = "/mpt",
+  needReloadOnImport = false,
+  needProfileKey = true,
+  preventRename = false,
+  willOverrideProfile = true,
+  nonNativeProfileString = false,
+  needSpecialInterface = false,
+  isLoaded = function(self)
+    local loaded = C_AddOns.IsAddOnLoaded("MPlusTimer")
+    return loaded
+  end,
+  isUpdated = function(self)
+    return private:GenericVersionCheck(self)
+  end,
+  needsInitialization = function(self)
+    return false
+  end,
+  openConfig = function(self)
+    if not SlashCmdList["ACECONSOLE_MPT"] then
+      return
+    end
+    SlashCmdList["ACECONSOLE_MPT"]()
+  end,
+  closeConfig = function(self)
+    SettingsPanel:Hide()
+  end,
+  getProfileKeys = function(self)
+    return MPTSV.Profiles
+  end,
+  getCurrentProfileKey = function(self)
+    local characterName = UnitName("player").."-"..GetNormalizedRealmName()
+    return MPTSV.ProfileKey and MPTSV.ProfileKey[characterName]
+  end,
+  isDuplicate = function(self, profileKey)
+    if not profileKey then return false end
+    return self:getProfileKeys()[profileKey] ~= nil
+  end,
+  setProfile = function(self, profileKey)
+    local characterName = UnitName("player").."-"..GetNormalizedRealmName()
+    UUFDB.profileKeys[characterName] = profileKey
+  end,
+  testImport = function(self, profileString, profileKey, profileData, rawData, moduleName)
+    if profileData and profileData.ChestTimer1 and profileData.ChestTimer2 then
+      return profileKey
+    end
+  end,
+  importProfile = function(self, profileString, profileKey, fromIntro)
+    if not profileString then return end
+    xpcall(function()
+      MPTAPI:ImportProfile(profileString, profileKey, true)
+    end, geterrorhandler())
+  end,
+  exportProfile = function(self, profileKey)
+    if not profileKey then return end
+    if type(profileKey) ~= "string" then return end
+    if not self:getProfileKeys()[profileKey] then return end
+    local export
+    xpcall(function()
+      export = MPTAPI:GetExportString(profileKey)
+    end, geterrorhandler())
+    return export
+  end,
+  areProfileStringsEqual = function(self, profileStringA, profileStringB, tableA, tableB)
+    if not profileStringA or not profileStringB then
+      return false
+    end
+    local _, _, profileDataA = private:GenericDecode(profileStringA)
+    local _, _, profileDataB = private:GenericDecode(profileStringB)
+    if not profileDataA or not profileDataB then
+      return false
+    end
+    return private:DeepCompareAsync(profileDataA, profileDataB)
+  end
+}
+private.modules[m.moduleName] = m
