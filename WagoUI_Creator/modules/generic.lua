0a621893cc16104fe386ae7d1b4d4fd6828faddc
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2026-03-17 17:51:29 +0100

    Add NaowhQOL LibAddonProfiles integration

--- WagoUI_Creator/modules/generic.lua
+++ WagoUI_Creator/modules/generic.lua
@@ -59,6 +59,7 @@ local defaultSortOrder = {
   "BuffReminders",
   "CooldownCursorManager",
   "Ayije_CDM",
+  "NaowhQOL",
   "atrocityEssentials",
   "NorskenUI",
 }
---
+++ WagoUI_Libraries/LibAddonProfiles/modules/NaowhQOL.lua
@@ -0,0 +1,101 @@
+local _, loadingAddonNamespace = ...
+---@type LibAddonProfilesPrivate
+local private = loadingAddonNamespace.GetLibAddonProfilesInternal and loadingAddonNamespace:GetLibAddonProfilesInternal()
+if (not private) then return end
+
+---@type LibAddonProfilesModule
+local m = {
+  moduleName = "NaowhQOL",
+  wagoId = "ZKxZvyNk",
+  oldestSupported = "20260316.03",
+  addonNames = { "NaowhQOL" },
+  conflictingAddons = {},
+  icon = C_AddOns.GetAddOnMetadata("NaowhQOL", "IconTexture"),
+  slash = "/nqol",
+  needReloadOnImport = true,
+  needProfileKey = true,
+  preventRename = false,
+  willOverrideProfile = true,
+  nonNativeProfileString = false,
+  needSpecialInterface = false,
+  isLoaded = function(self)
+    local loaded = C_AddOns.IsAddOnLoaded("NaowhQOL")
+    return loaded
+  end,
+  isUpdated = function(self)
+    return private:GenericVersionCheck(self)
+  end,
+  needsInitialization = function(self)
+    return false
+  end,
+  openConfig = function(self)
+    xpcall(function()
+      NaowhQOL_API:OpenConfig()
+    end, geterrorhandler())
+  end,
+  closeConfig = function(self)
+    xpcall(function()
+      NaowhQOL_API:CloseConfig()
+    end, geterrorhandler())
+  end,
+  getProfileKeys = function(self)
+    local profileKeys = {}
+    xpcall(function()
+      profileKeys = NaowhQOL_API:GetProfileKeys() or {}
+    end, geterrorhandler())
+    return profileKeys
+  end,
+  getCurrentProfileKey = function(self)
+    local profileKey
+    xpcall(function()
+      profileKey = NaowhQOL_API:GetCurrentProfileKey()
+    end, geterrorhandler())
+    return profileKey
+  end,
+  isDuplicate = function(self, profileKey)
+    if not profileKey then return false end
+    return self:getProfileKeys()[profileKey] ~= nil
+  end,
+  setProfile = function(self, profileKey)
+    if not profileKey then return end
+    if not self:getProfileKeys()[profileKey] then return end
+    xpcall(function()
+      NaowhQOL_API:SetProfile(profileKey)
+    end, geterrorhandler())
+  end,
+  testImport = function(self, profileString, profileKey, profileData, rawData, moduleName)
+
+  end,
+  importProfile = function(self, profileString, profileKey, fromIntro)
+    if not profileString then return end
+    xpcall(function()
+      NaowhQOL_API:ImportProfile(profileString, profileKey)
+    end, geterrorhandler())
+  end,
+  exportProfile = function(self, profileKey)
+    if not profileKey then return end
+    if type(profileKey) ~= "string" then return end
+    if not self:getProfileKeys()[profileKey] then return end
+    local export
+    xpcall(function()
+      export = NaowhQOL_API:ExportProfile(profileKey)
+    end, geterrorhandler())
+    return export
+  end,
+  areProfileStringsEqual = function(self, profileStringA, profileStringB, tableA, tableB)
+    if not profileStringA or not profileStringB then
+      return false
+    end
+    local profileDataA, profileDataB
+    xpcall(function()
+      profileDataA = NaowhQOL_API:DecodeProfileString(profileStringA)
+      profileDataB = NaowhQOL_API:DecodeProfileString(profileStringB)
+    end, geterrorhandler())
+    if not profileDataA or not profileDataB then
+      return false
+    end
+    return private:DeepCompareAsync(profileDataA, profileDataB)
+  end
+}
+
+private.modules[m.moduleName] = m
